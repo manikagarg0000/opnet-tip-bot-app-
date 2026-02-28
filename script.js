355b782d30e2b77f60c3d15700d4e0cddde0309d
Sadat BnB <manikagarg0000@gmail.com>
2026-02-28 17:15:27 +0600

    script.js

--- script.js
+++ script.js
@@ -6,9 +6,14 @@ function isWalletInstalled() {
 // Send Tip
 function sendTip() {
   if (isWalletInstalled()) {
-    window.opnet.sendBitcoin("recipientAddressHere", 1000, 1)
-      .then(tx => alert("Tip sent! TX: " + tx))
-      .catch(err => alert("Error: " + err));
+    window.opnet.sendBitcoin(
+      "opt1p9h9xzq9e0lcpkenag03ydq2y43y7pgxf7jwuusu2ajchlkaaxf8sseujqh", // your testnet address
+      1000, // sats
+      1,    // feeRate
+      { memo: "Tip from OP_NET Tip Bot" } // metadata for popup
+    )
+    .then(tx => alert("Tip sent! TX: " + tx))
+    .catch(err => alert("Error: " + err));
   } else {
     alert("OP_Wallet not installed!");
   }
@@ -17,45 +22,70 @@ function sendTip() {
 // Mint NFT
 function mintNFT() {
   if (isWalletInstalled()) {
-    window.opnet.sendBitcoin("contractAddressHere", 2000, 1, { action: "mintNFT", image: "https://opnet.org/sample.png" })
-      .then(tx => alert("NFT minted! TX: " + tx))
-      .catch(err => alert("Error: " + err));
+    window.opnet.sendBitcoin(
+      "opt1p9h9xzq9e0lcpkenag03ydq2y43y7pgxf7jwuusu2ajchlkaaxf8sseujqh",
+      2000,
+      1,
+      { action: "mintNFT", image: "https://opnet.org/sample.png" }
+    )
+    .then(tx => alert("NFT minted! TX: " + tx))
+    .catch(err => alert("Error: " + err));
   }
 }
 
 // Deploy Token
 function deployToken() {
   if (isWalletInstalled()) {
-    window.opnet.sendBitcoin("contractAddressHere", 3000, 1, { action: "deployToken", name: "MyToken", symbol: "MTK", supply: 1000000 })
-      .then(tx => alert("Token deployed! TX: " + tx))
-      .catch(err => alert("Error: " + err));
+    window.opnet.sendBitcoin(
+      "opt1p9h9xzq9e0lcpkenag03ydq2y43y7pgxf7jwuusu2ajchlkaaxf8sseujqh",
+      3000,
+      1,
+      { action: "deployToken", name: "MyToken", symbol: "MTK", supply: 1000000 }
+    )
+    .then(tx => alert("Token deployed! TX: " + tx))
+    .catch(err => alert("Error: " + err));
   }
 }
 
 // Buy Token
 function buyToken() {
   if (isWalletInstalled()) {
-    window.opnet.sendBitcoin("contractAddressHere", 1500, 1, { action: "buyToken" })
-      .then(tx => alert("Token bought! TX: " + tx))
-      .catch(err => alert("Error: " + err));
+    window.opnet.sendBitcoin(
+      "opt1p9h9xzq9e0lcpkenag03ydq2y43y7pgxf7jwuusu2ajchlkaaxf8sseujqh",
+      1500,
+      1,
+      { action: "buyToken" }
+    )
+    .then(tx => alert("Token bought! TX: " + tx))
+    .catch(err => alert("Error: " + err));
   }
 }
 
 // Sell Token
 function sellToken() {
   if (isWalletInstalled()) {
-    window.opnet.sendBitcoin("contractAddressHere", 1500, 1, { action: "sellToken" })
-      .then(tx => alert("Token sold! TX: " + tx))
-      .catch(err => alert("Error: " + err));
+    window.opnet.sendBitcoin(
+      "opt1p9h9xzq9e0lcpkenag03ydq2y43y7pgxf7jwuusu2ajchlkaaxf8sseujqh",
+      1500,
+      1,
+      { action: "sellToken" }
+    )
+    .then(tx => alert("Token sold! TX: " + tx))
+    .catch(err => alert("Error: " + err));
   }
 }
 
 // Claim Token
 function claimToken() {
   if (isWalletInstalled()) {
-    window.opnet.sendBitcoin("contractAddressHere", 1000, 1, { action: "claimToken" })
-      .then(tx => alert("Token claimed! TX: " + tx))
-      .catch(err => alert("Error: " + err));
+    window.opnet.sendBitcoin(
+      "opt1p9h9xzq9e0lcpkenag03ydq2y43y7pgxf7jwuusu2ajchlkaaxf8sseujqh",
+      1000,
+      1,
+      { action: "claimToken" }
+    )
+    .then(tx => alert("Token claimed! TX: " + tx))
+    .catch(err => alert("Error: " + err));
   }
 }
 
